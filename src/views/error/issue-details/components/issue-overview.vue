99be44bdd942ef92a3f1f54bb41ea3a255fd24e5
RoseBud13 <xiongkaijie13@gmail.com>
2022-08-02 17:11:53 +0800

    feat(error): 增加大致error详情页布局、组件和mock数据

---
+++ src/views/error/issue-details/components/issue-overview.vue
@@ -0,0 +1,32 @@
+<template>
+  <a-col class="issue-overview">
+    <a-col :span="8">
+      <a-typography-title :heading="4" style="margin-top: 0">
+        {{ issueInfo.name }}
+      </a-typography-title>
+      <a-typography-text code>
+        {{ issueInfo.from }}
+      </a-typography-text>
+    </a-col>
+  </a-col>
+</template>
+
+<script lang="ts" setup>
+  interface IIssueInfo {
+    name: string;
+    from: string;
+  }
+  const issueInfo: IIssueInfo = {
+    name: 'TypeError',
+    from: 'call(components/App)',
+  };
+</script>
+
+<style scoped lang="less">
+  .issue-overview {
+    width: 30%;
+    padding: 20px 20px 0 20px;
+    background-color: var(--color-bg-2);
+    border-radius: 4px 4px 0 0;
+  }
+</style>
--- src/views/error/issue-details/index.vue
+++ src/views/error/issue-details/index.vue
@@ -1,10 +1,24 @@
 <template>
   <div class="container">
     <Breadcrumb :items="['menu.error', 'menu.error.issueDetails']" />
+    <div class="overview-box">
+      <div class="panel">
+        <DataPanel></DataPanel>
+      </div>
+    </div>
+    <div class="main-box">
+      <div class="details">
+        <StackInfo></StackInfo>
+      </div>
+      <div class="statistics"></div>
+    </div>
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+  import DataPanel from './components/data-panel.vue';
+  import StackInfo from './components/stack-info.vue';
+</script>
 
 <script lang="ts">
   export default {
@@ -14,6 +28,40 @@
 
 <style scoped lang="less">
   .container {
-    padding: 0 20px 20px 20px;
+    display: flex;
+    flex-direction: column;
+    padding: 16px 20px;
+    padding-bottom: 0;
+    overflow: auto;
+    background-color: var(--color-fill-2);
+  }
+
+  .overview-box {
+    flex: 1;
+    margin-bottom: 10px;
+  }
+
+  .panel {
+    overflow: auto;
+    background-color: var(--color-bg-2);
+    border-radius: 4px;
+  }
+
+  :deep(.panel-border) {
+    margin-bottom: 0;
+    border-bottom: 1px solid rgb(var(--gray-2));
+  }
+
+  .main-box {
+    display: flex;
+    min-height: auto;
+  }
+
+  .details {
+    flex: 2;
+  }
+
+  .statistics {
+    flex: 1;
   }
 </style>
