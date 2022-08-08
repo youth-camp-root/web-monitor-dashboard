74fab97b1b2f6c0aac2d28e9117fc95283d6453c
xiaotianxt <tianyp@pku.edu.cn>
2022-08-09 00:18:34 +0800

    feat(user-detail): add UserCharts.vue framework

---
+++ src/views/user/[id]/components/charts/PageChart.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="echarts"> User Chart </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ src/views/user/[id]/components/charts/RequestChart.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="echarts"> Request Chart </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
