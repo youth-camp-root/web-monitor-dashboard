aa7e68acb8abb39067352f0565ab541e8ab665a2
gongheng <754097983@qq.com>
2022-08-10 22:57:12 +0800

    feat(issue-detail):init

--- src/views/error/issue-details/components/data-panel.vue
+++ src/views/error/issue-details/components/data-panel.vue
@@ -1,116 +1,89 @@
 <template>
-  <a-grid :cols="24" :row-gap="16" class="panel">
-    <a-grid-item
-      class="panel-col"
-      :span="{ xs: 12, sm: 12, md: 12, lg: 12, xl: 12, xxl: 6 }"
-    >
-      <a-space>
+  <a-row class="container">
+    <a-row class="grid-row">
+      <a-col class="grid-col">
         <IssueOverview></IssueOverview>
-      </a-space>
-    </a-grid-item>
-    <a-grid-item
-      class="panel-col"
-      :span="{ xs: 12, sm: 12, md: 12, lg: 12, xl: 12, xxl: 6 }"
-    >
-      <a-space>
-        <a-avatar :size="54" class="col-avatar">
-          <img
-            alt="avatar"
-            src="//p3-armor.byteimg.com/tos-cn-i-49unhts6dw/fdc66b07224cdf18843c6076c2587eb5.svg~tplv-49unhts6dw-image.image"
-          />
-        </a-avatar>
+      </a-col>
+    </a-row>
+    <a-row class="grid-row">
+      <a-col class="grid-col" :span="8">
         <a-statistic
-          title="Events"
+          :title="$t('error.issueDetails.errTotalNum')"
           :value="368"
           :value-from="0"
           animation
           show-group-separator
         >
         </a-statistic>
-      </a-space>
-    </a-grid-item>
-    <a-grid-item
-      class="panel-col"
-      :span="{ xs: 12, sm: 12, md: 12, lg: 12, xl: 12, xxl: 6 }"
-    >
-      <a-space>
-        <a-avatar :size="54" class="col-avatar">
-          <!-- <img
-            alt="avatar"
-            src="//p3-armor.byteimg.com/tos-cn-i-49unhts6dw/77d74c9a245adeae1ec7fb5d4539738d.svg~tplv-49unhts6dw-image.image"
-          /> -->
-          <icon-user-group />
-        </a-avatar>
+      </a-col>
+      <a-col :span="8">
         <a-statistic
-          title="Affected Users"
+          :title="$t('error.issueDetails.affectNum')"
           :value="74"
           :value-from="0"
           animation
           show-group-separator
         >
         </a-statistic>
-      </a-space>
-    </a-grid-item>
-    <a-grid-item
-      class="panel-col"
-      :span="{ xs: 12, sm: 12, md: 12, lg: 12, xl: 12, xxl: 6 }"
-      style="border-right: none"
-    >
-      <a-space>
-        <a-avatar :size="54" class="col-avatar">
-          <img
-            alt="avatar"
-            src="//p3-armor.byteimg.com/tos-cn-i-49unhts6dw/c8b36e26d2b9bb5dbf9b74dd6d7345af.svg~tplv-49unhts6dw-image.image"
-          />
-        </a-avatar>
+      </a-col>
+      <a-col class="grid-col" :span="8">
         <a-statistic
-          title="Trend"
+          :title="$t('error.issueDetails.errRate')"
           :value="2.8"
           :precision="1"
           :value-from="0"
           animation
         >
-          <template #suffix> % <icon-caret-up class="up-icon" /> </template>
+          <template #suffix> % </template>
         </a-statistic>
-      </a-space>
-    </a-grid-item>
-    <a-grid-item :span="24">
-      <a-divider class="panel-border" />
-    </a-grid-item>
-  </a-grid>
+      </a-col>
+    </a-row>
+    <a-row class="grid-row">
+      <Vcharts :options="options" :height="'300px'"></Vcharts>
+    </a-row>
+  </a-row>
 </template>
 
 <script lang="ts" setup>
+  import Vcharts from '@/components/chart/index.vue';
+
   import IssueOverview from './issue-overview.vue';
+
+  const options = {
+    title: { text: '错误次数统计表' },
+    tooltip: {},
+    xAxis: {
+      data: [
+        '2022-8-2',
+        '2022-8-3',
+        '2022-8-4',
+        '2022-08-5',
+        '2022-8-6',
+        '2022-8-7',
+      ],
+    },
+    yAxis: {},
+    series: [
+      {
+        name: '错误次数',
+        type: 'bar',
+        data: [5, 20, 36, 10, 10, 20],
+      },
+    ],
+  };
 </script>
 
-<style lang="less" scoped>
-  .arco-grid.panel {
-    margin-bottom: 0;
-    padding: 16px 20px 0 20px;
+<style scoped>
+  .container {
+    padding: 20px;
   }
 
-  .panel-col {
-    padding-left: 43px;
-    border-right: 1px solid rgb(var(--gray-2));
+  .grid-row {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 20px;
   }
 
-  .col-avatar {
-    margin-right: 12px;
-    background-color: var(--color-fill-2);
-  }
-
-  .up-icon {
-    color: rgb(var(--red-6));
-  }
-
-  .unit {
-    margin-left: 8px;
-    color: rgb(var(--gray-8));
-    font-size: 12px;
-  }
-
-  :deep(.panel-border) {
-    margin: 4px 0 0 0;
-  }
+  /* echarts */
 </style>
--- src/views/error/issue-details/locale/en-US.ts
+++ src/views/error/issue-details/locale/en-US.ts
@@ -2,4 +2,7 @@ export default {
   'menu.error.issueDetails': 'Issue Details',
   'error.issueDetails.stackInfo': 'Stack Info',
   'error.issueDetails.viewMore': 'View more',
+  'error.issueDetails.errTotalNum':'errTotalNum',
+  'error.issueDetails.affectNum':'affectNum',
+  'error.issueDetails.errRate':'errRate',
 };
