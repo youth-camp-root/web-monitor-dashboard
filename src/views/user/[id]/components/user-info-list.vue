394c70b61504d8f77adcd6c6c7ddb486db9850da
xiaotianxt <tianyp@pku.edu.cn>
2022-08-10 19:30:22 +0800

    feat(user-detail): add user-info-list framework

--- src/views/user/[id]/components/user-info-list.vue
+++ src/views/user/[id]/components/user-info-list.vue
@@ -1,7 +1,88 @@
 <template>
-  <div> 列表 </div>
+  <a-table :columns="columns" :data="data" @change="handleChange">
+    <template #type>
+      <a-tag color="arcoblue"> {{ $t('user.detail.list.type.event') }} </a-tag>
+      <a-tag color="red"> {{ $t('user.detail.list.type.error') }} </a-tag>
+    </template>
+    <template
+      #name-filter="{
+        filterValue,
+        setFilterValue,
+        handleFilterConfirm,
+        handleFilterReset,
+      }"
+    >
+      <div class="custom-filter">
+        <a-space direction="vertical">
+          <a-input
+            :model-value="filterValue[0]"
+            @input="(value: string) => setFilterValue([value])"
+          />
+          <div class="custom-filter-footer">
+            <a-button @click="handleFilterConfirm">Confirm</a-button>
+            <a-button @click="handleFilterReset">Reset</a-button>
+          </div>
+        </a-space>
+      </div>
+    </template>
+  </a-table>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import { h } from 'vue';
+  import { IconSearch } from '@arco-design/web-vue/es/icon';
+  import {
+    TableChangeExtra,
+    TableColumnData,
+    TableData,
+  } from '@arco-design/web-vue';
+  import { useI18n } from 'vue-i18n';
+  import { userInfoList } from '@/views/user/[id]/mock';
 
-<style scoped></style>
+  const { t } = useI18n();
+
+  const columns: TableColumnData[] = [
+    {
+      title: t('user.detail.list.type'),
+      dataIndex: 'type',
+      slotName: 'type',
+    },
+    {
+      title: t('user.detail.list.datetime'),
+      dataIndex: 'datetime',
+      filterable: {
+        filter: (value: string[], record: TableData) =>
+          record.name.includes(value),
+        slotName: 'name-filter',
+        icon: () => h(IconSearch),
+      },
+    },
+    {
+      title: 'Salary',
+      dataIndex: 'salary',
+      sortable: {
+        sortDirections: ['ascend'],
+      },
+    },
+  ];
+  const data: any = userInfoList;
+
+  const handleChange = (table: TableData[], extra: TableChangeExtra) => {
+    console.log('change', table, extra);
+  };
+</script>
+
+<style scoped lang="less">
+  .custom-filter {
+    padding: 20px;
+    background: var(--color-bg-5);
+    border: 1px solid var(--color-neutral-3);
+    border-radius: var(--border-radius-medium);
+    box-shadow: 0 2px 5px rgb(0 0 0 / 10%);
+  }
+
+  .custom-filter-footer {
+    display: flex;
+    justify-content: space-between;
+  }
+</style>
--- src/views/user/[id]/mock.ts
+++ src/views/user/[id]/mock.ts
@@ -2,6 +2,7 @@ import setupMock, { successResponseWrap } from '@/utils/setup-mock';
 import { UserInfo } from '@/api/user';
 
 import Mock, { Random } from 'mockjs';
+import { TableData } from '@arco-design/web-vue';
 
 export const userInfo: UserInfo = Mock.mock({
   objectId: Random.integer(1.84467441e19).toString(16),
@@ -11,6 +12,15 @@ export const userInfo: UserInfo = Mock.mock({
   ip: '@ip',
 });
 
+export const userInfoList: TableData[] = Mock.mock({
+  'list|1-10': [
+    {
+      'key|1-10': 1,
+      'datetime': '@DATE',
+      'salary': 23000,
+    },
+  ],
+}).list;
 export const otherInfo = {}; // TODO: add other info
 
 setupMock({
