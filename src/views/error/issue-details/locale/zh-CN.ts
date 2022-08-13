b420aa7be28ce4b567214c2d187b5f040191d1e7
RoseBud13 <32515593+RoseBud13@users.noreply.github.com>
2022-08-13 20:13:11 +0800

    chore(reformat): format for eslint checking (#15)

--- src/views/error/issue-details/locale/zh-CN.ts
+++ src/views/error/issue-details/locale/zh-CN.ts
@@ -2,7 +2,7 @@ export default {
   'menu.error.issueDetails': '异常详情',
   'error.issueDetails.stackInfo': '堆栈信息',
   'error.issueDetails.viewMore': '查看更多',
-  'error.issueDetails.errTotalNum':'总发生次数',
-  'error.issueDetails.affectNum':'影响用户数',
-  'error.issueDetails.errRate':'错误发生率',
+  'error.issueDetails.errTotalNum': '总发生次数',
+  'error.issueDetails.affectNum': '影响用户数',
+  'error.issueDetails.errRate': '错误发生率',
 };
