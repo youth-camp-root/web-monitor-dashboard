6755d3259a6cdd06f0d2e24bf53d8a1610c841df
xiaotianxt <tianyp@pku.edu.cn>
2022-08-17 17:01:54 +0800

    feat(user): finish user info api

--- src/views/user/[id]/index.vue
+++ src/views/user/[id]/index.vue
@@ -1,29 +1,45 @@
 <template>
   <div class="container">
     <Breadcrumb :items="['menu.user', 'menu.user.detail']" />
-    <a-space direction="vertical" :size="16" fill>
-      <a-card :title="$t('user.detail.userInfo.title')">
-        <UserInfo :id="id" />
-      </a-card>
-      <a-card :title="$t('user.detail.userCharts.title')">
-        <UserCharts />
-      </a-card>
-      <a-card :title="$t('user.detail.userInfoList.title')">
-        <UserInfoList />
-      </a-card>
-    </a-space>
+    <a-spin :loading="loading">
+      <a-space direction="vertical" :size="16" fill>
+        <a-card :title="$t('user.detail.userInfo.title')">
+          <UserInfo :id="id" :data="data.user" />
+        </a-card>
+        <a-card :title="$t('user.detail.userCharts.title')">
+          <UserCharts />
+        </a-card>
+        <a-card :title="$t('user.detail.userInfoList.title')">
+          <UserInfoList />
+        </a-card>
+      </a-space>
+    </a-spin>
   </div>
 </template>
 
 <script setup lang="ts">
   import { useRoute } from 'vue-router';
   import Breadcrumb from '@/components/breadcrumb/index.vue';
-  import UserInfo from './components/user-info.vue';
-  import UserCharts from './components/user-charts.vue';
-  import UserInfoList from './components/user-info-list.vue';
+  import useRequest from '@/hooks/request';
+  import { IUserFull, queryUserInfo } from '@/api/user';
+  import UserInfo from '@/views/user/[id]/components/user-info.vue';
+  import UserCharts from '@/views/user/[id]/components/user-charts.vue';
+  import UserInfoList from '@/views/user/[id]/components/user-info-list.vue';
+  import { IErrorData } from '@/api/errorData';
+  import { IRequestData } from '@/api/requestData';
+
+  type IUserRequest = {
+    user: IUserFull;
+    errors: IErrorData[];
+    events: IRequestData[];
+  };
 
   const route = useRoute();
   const id = route.params.id as string;
+
+  const { loading, response: data } = useRequest<IUserRequest>(() =>
+    queryUserInfo(id)
+  );
 </script>
 
 <script lang="ts">
